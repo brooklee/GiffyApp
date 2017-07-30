7153b4ce61c311c04b5fb3c3e92081b0a8fd41dc
brooklee1 <brooklee.wilson@gmail.com>
2017-07-30 13:09:52 -0600

    typo fix

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,5 @@
 
-// Initial array of animals
+// Initial array of animals.
   var animals = ["dog", "cat", "rabbit", "hamster", "skunk", "goldfish", "bird", "ferret", "turtle", "sugar glider", "chinchilla", "hedgehog", "hermit crab", "gerbil", "pygmy goat", "chicken", "capybara","teacup pig", "serval", "salamander", "frog"];
 
 
